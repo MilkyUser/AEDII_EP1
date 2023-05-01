5f80c8739d933adc18b17815a199a67128bb91e2
MilkyUser <brunoslno@hotmail.com>
2023-05-01 18:21:05 -0300

    ASCII alphabet graph generation added

--- generator.py
+++ generator.py
@@ -4,14 +4,27 @@ from sys import argv
 import random
 
 
-def generate_graph(v: int, a: int)->dict: # returns a dict with the structure <<str>: [<str>, ...], ...>
+def generate_graph(v: int, a: int, alphabet: str = 'abc')->dict: # returns a dict with the structure <<str>: [<str>, ...], ...>
 	
 	graph = dict()
 	
-	with open('./Wordlists-PT-BR/nomes.txt', 'r') as names_file:
-		no_lines = sum(1 for line in names_file)		
-		for _ in range(v):
-			graph[getline(names_file.name, random.randrange(no_lines)).replace('\n', '')] = []
+	if v < 1:
+		raise ValueError(f'número mínimo de vértices \'v\' = 1')
+	
+	if alphabet.upper() == 'NOMES':
+		with open('./Wordlists-PT-BR/nomes.txt', 'r') as names_file:
+			no_lines = sum(1 for line in names_file)		
+			for _ in range(v):
+				graph[getline(names_file.name, random.randrange(no_lines)).replace('\n', '')] = []
+	elif alphabet.upper() == 'ABC':
+		if v > 26*2:
+			raise ValueError(f'número máximo de vértices para modo "abc" = {26*2}')
+		if v > 26:
+			char_range = list(range(97, 123)) + list(range(65, 65+v-26))
+		if v <= 26:
+			char_range = list(range(97, 97+v))
+		for val in char_range:
+				graph[chr(val)] = []
 	
 	dist = lambda size, count: [(elem*size)//1 for elem in dirichlet(
 		[elem if elem > 0 else 0.01 for elem in normal(size=count, loc=1, scale=25/count)]
@@ -31,7 +44,11 @@ def generate_graph(v: int, a: int)->dict: # returns a dict with the structure <<
 
 
 if __name__ == '__main__':
-	graph = generate_graph(int(argv[1]), int(argv[2]))
+	if len(argv) >= 4:
+		alphabet = argv[3]
+	else:
+		alphabet = 'abc' 
+	graph = generate_graph(int(argv[1]), int(argv[2]), alphabet)
 	print(len(graph))
 	for key, value in graph.items():
 		print(key + ": " + "; ".join(value), end='')
